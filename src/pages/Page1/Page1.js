d77afb1cd73c79ffd6522f32f52dd19fbd0da98a
张文 <zhangwen@zhangwendeMacBook-Pro.local>
2018-11-12 23:11:42 +0800

    webpack

---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./Page1.css";
+import image from './images/1.jpg';
+export default class Page1 extends Component {
+    render() {
+        return (
+            <div className="box">
+                <img src={image}/>
+ 
+                this is Page1~11    
+            </div>
+        )
+    }
+}
