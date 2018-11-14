4a723a9024c9f57e37d8e9bbb9d22a8803166938
zjhjszwx <wenxin.zhang@mobilemd.cn>
2018-11-14 08:50:09 +0800

    1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import {Button,DatePicker} from 'antd';
-=======
-import './index.css';
->>>>>>> d40271a5f5eebb8fd28750cfc21782547d315ff3
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -21,11 +17,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
-                this is 2~<br/>
-=======
                 this is home111~<br/>
->>>>>>> d40271a5f5eebb8fd28750cfc21782547d315ff3
                 当前计数：{this.state.count}<br/>
                 <button onClick={() => this._handleClick()}>自增</button>
                 <li>
