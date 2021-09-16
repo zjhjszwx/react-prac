e68524271b92bf95156233adbc752534401f14e3
zhangwenxin <17830723072@dingtalk.com>
2021-09-16 16:49:32 +0800

    目录调整

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,4 @@
 import React, { Component } from 'react';
-import Clock from './Clock';
-import withTimer from './withTimer';
-import TabSelectSimple from './TabSelector';
-import Think from './ThinkingInReact';
-import Mobile from '../../component/hightOrder/Mobile';
-import Username from '../../component/hightOrder/Username';
-import Context from './Context';
-import ReactPatten from './reactPattern';
-import UseDefined from '../Hooks/useDefined.js';
-import UseStateDemo from '../Hooks/useStateDemo';
-import UseEffect from '../Hooks/useEffect';
-import UseRef from '../Hooks/useRef';
-import UseReducer from '../Hooks/useReducer';
-import UseContext from '../Hooks/useContext';
-import SyntheticEvent from '../lifeCycle/SyntheticEvent';
-import Key from '../Key/index';
-import MTable from '../component/Table';
-import TSelect from '../component/Select';
-import Memo1 from '../Hooks/memo';
-import ResilientComponents from '../danBlog/resilientComponents';
-import Test from '../PanziEditor/Test.js';
-import VirtualList from '../../component/VirtualList';
-import TimeTest from '../../component/VirtualList/TimeTest';
 
 class Home extends Component {
   state = {
@@ -29,11 +6,7 @@ class Home extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <VirtualList />
-      </div>
-    );
+    return <div>123</div>;
   }
 }
 export default Home;
